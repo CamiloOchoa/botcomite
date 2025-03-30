51e597c4cbe38309de175f447af6eef021d6d05d
CamiloOchoa <32837611+CamiloOchoa@users.noreply.github.com>
2025-03-31 01:36:20 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Iniciar bot de Telegram en segundo plano
-python3 -m main &
-# Iniciar Gunicorn para Flask
-gunicorn --worker-tmp-dir /dev/shm --workers 4 --timeout 120 --bind 0.0.0.0:$PORT main:app
